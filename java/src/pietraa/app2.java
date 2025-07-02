ef06b1cac7df87978cfdafa8619620c577f86524
pyxie142 <pietra.almeida.nunes@escola.pr.gov.br>
2025-07-02 09:25:49 -0300

    hghg

---
+++ java/src/pietraa/app2.java
@@ -0,0 +1,15 @@
+package pietraa;
+
+public class app2 {
+    public static void main(String[] args) {
+        aula livro = new aula();
+        livro.nome = "absolute superman";
+        livro.descricao = "uma história em quadrinhos de super-herói";
+        livro.isbn = "88711741";
+        livro.preco = 99.88;
+        livro.autor = "Jason Aaron";
+        livro.datapub = "2024";
+        System.out.println(livro); 
+
+    }
+}
---
+++ java/src/pietraa/aula.java
@@ -0,0 +1,15 @@
+package pietraa;
+
+public class aula{
+
+String nome;
+String descricao;
+String autor;
+String isbn;
+double preco;
+String datapub;
+
+
+}
+
+
